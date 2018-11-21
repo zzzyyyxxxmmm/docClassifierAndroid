add428b3e794f936f2ed1322b4bee4f1904adc02
jikangwang <wjk32111@gmail.com>
2018-11-20 23:14:15 -0500

    [fix] change URL

--- app/src/main/java/com/wjk32/docclassifier/Constants.java
+++ app/src/main/java/com/wjk32/docclassifier/Constants.java
@@ -4,5 +4,5 @@ package com.wjk32.docclassifier;
  * Created by Jikang Wang on 11/16/18.
  */
 public class Constants {
-    public static final String URL="http://flask-env.8x6jvjnvrc.us-east-2.elasticbeanstalk.com/";
+    public static final String URL="http://flask-doc.sgn7ieqpcn.us-east-2.elasticbeanstalk.com/";
 }
